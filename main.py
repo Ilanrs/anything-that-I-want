74a0c0261b3ccaa77bc751644bbd3e9caed4354a
cwklinus22015 <student+22015@codingwithkids.com>
2019-08-21 14:58:40 -0700

    I have the color and the ship

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
 import random
-import pandas as pd
-from ship import Ship
-from asteroid import Asteroid
+# import pandas as pd
+from ship import *
+# from asteroid import Asteroid
 from pygame.locals import *
 
 pygame.init()
@@ -12,7 +12,7 @@ screen_info = pygame.display.Info()
 size = (width, height) = (int(screen_info.current_w* 0.5), int(screen_info.current_h* 0.5))
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-color = (30, 0, 30)
+color = (192, 192, 192)
 screen.fill(color)
 
 # df = pd.read_csv('game_info.csv')
@@ -23,17 +23,20 @@ Level = 1
 AsteroidCount = 3
 Player = Ship((20, 200))
 
+
 def main():
-    global level
+    global Level
 
     while Level <= NumLevels:
         clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == QUIT
-            sys.exit
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
             screen.fill(color)
-            screen.blit(Plqyer.image, Player.rect)
+            screen.blit(Player.image, Player.rect)
             pygame.display.flip()
 
-if __name__ == '__name__':
-        main()
+
+if __name__ == '__main__':
+    main()
